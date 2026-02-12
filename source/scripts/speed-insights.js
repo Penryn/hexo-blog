aab2ee4839df51432747f43173d87eeea826d3cf
Penryn <15158052130@163.com>
2026-02-13 00:43:14 +0800

    perf

--- source/scripts/speed-insights.js
+++ source/scripts/speed-insights.js
@@ -1,2 +1,2 @@
-import { injectSpeedInsights } from 'https://unpkg.com/@vercel/speed-insights@latest/dist/index.mjs';
+import { injectSpeedInsights } from 'https://unpkg.com/@vercel/speed-insights@1.3.1/dist/index.mjs';
 injectSpeedInsights();
